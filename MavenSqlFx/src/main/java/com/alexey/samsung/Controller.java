c1357f349319f5ab7a9060eb1f6fe500c69a8728
aoklyunin <aoklyunin@gmail.com>
2016-09-24 23:38:48 +0300

    1

---
+++ MavenSqlFx/src/main/java/com/alexey/samsung/Controller.java
@@ -0,0 +1,23 @@
+package com.alexey.samsung;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+public class Controller {
+    @FXML
+    private Button button;
+
+    @FXML
+    public void initialize(){
+        button.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                DBHelper dbHelper = new DBHelper();
+                dbHelper.open();
+            }
+        });
+    }
+
+
+}
