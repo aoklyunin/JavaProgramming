f3df4d44c3125f339cbb1cc40378d5bc2c2dd8cc
aoklyunin <aoklyunin@gmail.com>
2016-09-24 20:26:32 +0300

    zx

---
+++ SamsungCom/src/main/java/com/alexey/samsung/Main.java
@@ -0,0 +1,45 @@
+package com.a
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class Main extends Application {
+
+    private Stage primaryStage;
+    private AnchorPane rootLayout;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("AddressApp");
+
+        initRootLayout();
+    }
+
+    public void initRootLayout() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("sample.fxml"));
+            rootLayout = (AnchorPane) loader.load();
+
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
---
+++ SamsungCom/src/main/java/com/alexey/samsung/controller/MainController.java
@@ -0,0 +1,7 @@
+package com.alexey.samsung.controller;
+
+/**
+ * Created by aokly on 24.09.2016.
+ */
+public class MainController {
+}
