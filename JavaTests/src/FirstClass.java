00e8f1fb2595de2db54e56bea329696cbb0737e3
aoklyunin <aoklyunin@gmail.com>
2016-10-01 15:40:58 +0300

    a

---
+++ JavaTests/src/FirstClass.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+        double answ;
+        int a =5;
+        int b =2;
+
+
+        answ = (double)a/b;
+
+        System.out.println(answ);
+    }
+}
--- JavaTests/src/Main.java
+++ JavaTests/src/Main.java
@@ -1,6 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Locale.setDefault(new Locale("en", "US"));
+       // System.err.println(Locale.getDefault());
+
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            double a = sc.nextDouble();
+            double b = sc.nextDouble();
+
+            //int b = sc.nextInt();
+            //double ln = Math.sqrt(a * a + b * b);
+            System.out.println(a+b);
+            System.out.println(a-b);
+            System.out.println(a*b);
+            System.out.println(a/b);
+            System.out.println(Math.pow(a,b));
+        }
+
+
     }
+
+
 }
+
